cb9b628b040fbeb9d4ce392923285e6c8514ff5e
Kiran Kandel <theknownofficial@gmail.com>
2021-07-07 00:27:12 +0545

    ädded model for posts

---
+++ models/Post.model.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+
+    const Post = sequelize.define('Post', {
+        title: {
+            type: DataTypes.STRING,
+             required: true,
+              allowNull: false
+            },
+        content: {
+            type: DataTypes.STRING,
+             required: true,
+              allowNull: false
+            },
+        date: {
+            type: DataTypes.DATE,
+            required: true,
+            allowNull: false,
+            default: Date.now()
+        },
+        author:{
+            type: DataTypes.STRING,
+            required: true,
+            allowNull: false}
+    }, {timestamps: false});
+
+    Post.associate = function (models) {
+        Post.belongsTo(models.User,{
+            foreignKey:'authorId',
+            onDelete:'CASCADE',
+        });
+    };
+
+    return Post;
+};
