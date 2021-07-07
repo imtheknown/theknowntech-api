42bb71919fea296185cf612bb76a0ae24da4df22
Kiran Kandel <theknownofficial@gmail.com>
2021-07-07 23:33:18 +0545

    refactor createpost controller and fix some typos

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async.middleware');
 exports.createPost = asyncHandler(async (req, res, next) => {
     let postCredentials = req.body;
     let errorMsg = '';
-    if (!req.isAuthenticated()) {
+    if (!req) {
         errorMsg = 'You should be logged post an article'
     } else if (!postCredentials.title) {
         errorMsg = 'Invalid title!';
@@ -16,9 +16,9 @@ exports.createPost = asyncHandler(async (req, res, next) => {
         res.status(404).json({ success: false, data: {message: errorMessage}});
         return;
     }
-    postCredentials.authorId = req.user.id;
+    postCredentials.authorId = req.body.user.id;
     postCredentials.date = Date.now();
-    postCredentials.author = req.user.username;
+    postCredentials.author = req.body.user.username;
     console.log(postCredentials);
     Post.create(postCredentials).then(post => {
         res.status(201).json({ success: true, data: post });
